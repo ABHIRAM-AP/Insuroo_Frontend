f59c7d50c3caa85bc9034460278ea0b1ae467427
Abhiram <abhiramap05@gmail.com>
2026-06-19 21:50:14 +0530

    feat(api_service): implemented getRecommendations func

--- insuroo/lib/services/api_service.dart
+++ insuroo/lib/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+import '../models/user_profile.dart';
+
 class ApiService {
   static const String _baseUrl = 'http://127.0.0.1:8000';
 
@@ -17,6 +19,23 @@ class ApiService {
       return data['answer'] as String;
     } else {
       throw Exception('Failed to get answer. Status: ${response.statusCode}');
+    }
+  }
+
+  Future<RecommendationResponse> getRecommendations(UserProfile profile) async {
+    final url = Uri.parse('$_baseUrl/query/recommend');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(profile.toJson()),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return RecommendationResponse.fromJson(data);
+    } else {
+      throw Exception(
+          'Failed to get recommendations. Status: ${response.statusCode}');
     }
   }
 
